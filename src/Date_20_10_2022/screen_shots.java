ed3d5b6e606bc8b47b98b1afd62aa1da73734aca
Sarthak <Sarthak@DESKTOP-EG5JKER>
2022-12-21 12:06:57 +0530

    secong Project

---
+++ src/Date_20_10_2022/screen_shots.java
@@ -0,0 +1,17 @@
+package Date_20_10_2022;
+
+public class screen_shots {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\16 july selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
